f1ceede49e8c4fa0d69c08040a781ec597243c99
Ricardo Salazar <therlokx@gmail.com>
2025-06-04 02:14:56 +0200

    expand inventory until step 7

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,3 +1,4 @@
+const baseURL = import.meta.env.VITE_SERVER_URL
 //export th path to get the information of the products
 function convertToJson(res) {
   if (res.ok) {
@@ -8,17 +9,18 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
+  constructor() {
+
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
-  }
+  async getData(category) {
+  const response = await fetch(`${baseURL}products/search/${category} `);
+  const data = await convertToJson(response);
+  return data.Result;
+}
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    const response = await fetch(`${baseURL}product/${id}`);
+    const data = await convertToJson(response);
+    console.log(data.Result);
+    return data.Result;
   }
 }
---
+++ src/js/product-listing.js
@@ -0,0 +1,14 @@
+import Alert from "./Alert.js";
+import {loadHeaderFooter, getParam} from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+const category = getParam('category');
+const dataSource = new ProductData();
+const element = document.querySelector(".product-list");
+const producList = new ProductList(category, dataSource, element);
+producList.init();
+
+loadHeaderFooter()
+const alert = new Alert("/json/alerts.json");
+alert.showAlerts();
