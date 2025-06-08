244f7877495675f4df3b5b5a4cad2f0440edd714
thelokx <therlokx@gmail.com>
2025-06-09 01:01:12 +0200

    Update product.js

--- src/js/product.js
+++ src/js/product.js
@@ -7,14 +7,3 @@ const dataSource = new ProductData("tents");
 const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-// // add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
