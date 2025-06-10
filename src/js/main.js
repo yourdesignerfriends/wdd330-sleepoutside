c5ff720723e227b2e9fdaac8300fc662e4b9a741
mariajosemackay <mariajoarevalon@gmail.com>
2025-06-10 02:19:40 -0600

    Add CTA modal on first visit to the website

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 import Alert from "./Alert.js";
 import {loadHeaderFooter } from "./utils.mjs";
+import setupCTAModal from "./cta-modal.js";
 
 loadHeaderFooter();
 
 const alert = new Alert("/json/alerts.json");
 alert.showAlerts();
 
-setupNewsletterForm();
+//setupNewsletterForm();
+
+setupCTAModal();
